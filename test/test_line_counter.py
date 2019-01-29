448f5532b2798ce1048f47daa85619b2bf297ccd
Matt Chernosky <matt@chernosky.net>
2019-01-28 22:00:24 -0700

    Handle block comments

--- test/test_line_counter.py
+++ test/test_line_counter.py
@@ -1,14 +1,21 @@
+import re
+
 import pytest
 
 
 class LineCounter:
     @staticmethod
     def count(string):
+        string = re.sub(r'/\*.*\*/', '', string, flags=re.DOTALL)
         lines = string.splitlines()
         # Remove blank lines.
         lines = [l for l in lines if not l.isspace()]
         # Remove comment lines.
         lines = [l for l in lines if not l.lstrip().startswith("//")]
+        # Remove block comment lines.
+        lines = [l for l in lines if not l.lstrip().startswith("/*")]
+        # Remove ending comment lines.
+        lines = [l for l in lines if not l.rstrip().endswith("*/")]
         return len(lines)
 
 
@@ -40,3 +47,37 @@ def test_dont_count_comment_lines():
     line_count = LineCounter.count(code)
     assert(line_count == 2)
 
+
+def test_count_lines_with_trailing_comments():
+    code = """int i = 0;
+              bool x = true; // a comment
+              int j = 1;"""
+    line_count = LineCounter.count(code)
+    assert(line_count == 3)
+
+
+def test_dont_count_single_line_block_comments():
+    code = """int i = 0;
+              /* a comment */
+              int j = 1;"""
+    line_count = LineCounter.count(code)
+    assert(line_count == 2)
+
+
+def test_dont_count_block_comments_spanning_two_lines():
+    code = """int i = 0;
+              /* a comment
+              that spans two lines */
+              int j = 1;"""
+    line_count = LineCounter.count(code)
+    assert(line_count == 2)
+
+
+def test_dont_count_block_comments_spanning_multiple_lines():
+    code = """int i = 0;
+              /* a comment
+              that spans
+              three lines */
+              int j = 1;"""
+    line_count = LineCounter.count(code)
+    assert(line_count == 2)
