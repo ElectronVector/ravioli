499c037f1d8088c9a8289b292825fd343a383be4
Matt Chernosky <matt@chernosky.net>
2019-01-29 22:55:55 -0700

    Start parsing function names

--- test/test_complexity.py
+++ test/test_complexity.py
@@ -1,14 +1,21 @@
+import re
+from pprint import pprint
+
 import pytest
 
 
 def calculate_complexity(code):
-    return {'a_function': 1}
+    results = {}
+    for line in code.splitlines():
+        m = re.search(r'\s+(\w+)\s*\(', line)
+        if m:
+            results[m.group(1)] = 1
+    return results
 
 
 def test_a_function_can_be_parsed():
     code = """
-            int a_function () {
-            }
+            int a_function(){}
             """
     results = calculate_complexity(code)
     assert ('a_function' in results)
@@ -16,14 +23,25 @@ def test_a_function_can_be_parsed():
 
 def test_two_functions_can_be_parsed():
     code = """
-            int a_function () {
-            }
-            
-            int another_function ()
-            {
-            }
+            int a_function(){}
+            int another_function(){}
             """
     results = calculate_complexity(code)
     assert ('a_function' in results)
     assert ('another_function' in results)
 
+
+def test_a_function_with_arguments_can_be_parsed():
+    code = """
+            int a_function(int a){}
+            """
+    results = calculate_complexity(code)
+    assert ('a_function' in results)
+
+
+def test_a_function_with_some_whitespace_can_be_parsed():
+    code = """
+            int a_function (int a){}
+            """
+    results = calculate_complexity(code)
+    assert ('a_function' in results)
