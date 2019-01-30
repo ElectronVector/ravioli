5cdca8f0769bffaa5870d18d6564f464f2dbd978
Matt Chernosky <matt@chernosky.net>
2019-01-29 21:34:16 -0700

    Refactoring

--- c_parser.py
+++ c_parser.py
@@ -1,7 +1,8 @@
+import re
+from subprocess import check_output
+
 import pycparser
 from pycparser import c_ast
-
-from ravioli import preprocess, sanitize
 
 
 class CustomVisitor(c_ast.NodeVisitor):
@@ -80,3 +81,51 @@ def parse(filename, include_paths):
     v.results['globals'] = global_variables
 
     return v.results
+
+
+def preprocess(filename, include_paths):
+    """ Preprocess a file using gcc as the preprocessor.
+
+        filename:
+            The name of the file (path to the file) to preprocess.
+
+        include_paths:
+            All the paths that files may be included from.
+
+        When successful, returns the contents of the preprocessed file.
+        Errors from the processor are printed.
+    """
+    cpp_path = 'gcc'
+    cpp_args = [r'-E', r'-Ifake_libc_include']
+    for path in include_paths:
+        cpp_args.append(f'-I{path}')
+
+    command = [cpp_path] + cpp_args + [filename]
+
+    try:
+        # Note the use of universal_newlines to treat all newlines
+        # as \n for Python's purpose
+        text = check_output(command, universal_newlines=True)
+    except OSError as e:
+        raise RuntimeError("Unable to invoke 'cpp'.  " +
+                           'Make sure its path was passed correctly\n' +
+                           ('Original error: %s' % e))
+
+    return text
+
+
+def sanitize(text):
+    """ Sanitize some C code be removing non-standard compiler extensions.
+
+        text:
+            The C code to sanitize.
+
+    """
+    text = text.replace('__interrupt', '')
+    text = text.replace('interrupt', '')
+    text = text.replace('*far', '*')
+    matcher = '@(___)'  # An example of what you might use.
+    matcher = re.escape(matcher)
+    matcher = matcher.replace('___', '.*')
+    text = re.sub(matcher, '', text)
+    return text
--- ravioli.py
+++ ravioli.py
@@ -1,60 +1,8 @@
-import re
 import sys
 from pathlib import Path
 
-from subprocess import check_output
-
 import c_parser
 from line_counter import LineCounter
-
-
-def preprocess(filename, include_paths):
-    """ Preprocess a file using gcc as the preprocessor.
-
-        filename:
-            The name of the file (path to the file) to preprocess.
-
-        include_paths:
-            All the paths that files may be included from.
-
-        When successful, returns the contents of the preprocessed file.
-        Errors from the processor are printed.
-    """
-    cpp_path = 'gcc'
-    cpp_args = [r'-E', r'-Ifake_libc_include']
-    for path in include_paths:
-        cpp_args.append(f'-I{path}')
-
-    command = [cpp_path] + cpp_args + [filename]
-
-    try:
-        # Note the use of universal_newlines to treat all newlines
-        # as \n for Python's purpose
-        text = check_output(command, universal_newlines=True)
-    except OSError as e:
-        raise RuntimeError("Unable to invoke 'cpp'.  " +
-                           'Make sure its path was passed correctly\n' +
-                           ('Original error: %s' % e))
-
-    return text
-
-
-def sanitize(text):
-    """ Sanitize some C code be removing non-standard compiler extensions.
-
-        text:
-            The C code to sanitize.
-
-    """
-    text = text.replace('__interrupt', '')
-    text = text.replace('interrupt', '')
-    text = text.replace('*far', '*')
-    matcher = '@(___)'  # An example of what you might use.
-    matcher = re.escape(matcher)
-    matcher = matcher.replace('___', '.*')
-    text = re.sub(matcher, '', text)
-    return text
-
 
 if __name__ == "__main__":
     folder = Path('./motobox')
