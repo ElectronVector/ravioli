fdb10891fd61fd3b31ca85283495992e9aaa8606
Matt Chernosky <matt@chernosky.net>
2019-01-31 22:00:28 -0700

    Handle if statements and other function calls

--- test/test_complexity.py
+++ test/test_complexity.py
@@ -7,9 +7,11 @@ import pytest
 def calculate_complexity(code):
     results = {}
     for line in code.splitlines():
-        m = re.search(r'\s+(\w+)\s*\(', line)
+        m = re.search(r'\s+(\w+)\s*\(.*\)\s*{', line)
         if m:
-            results[m.group(1)] = 1
+            name = m.group(1)
+            if name != 'if':
+                results[name] = 1
     return results
 
 
@@ -52,6 +54,20 @@ def test_a_function_with_an_if_can_be_parsed():
             int a_function (int a){
                 if (a > 5) {
                     return 0;
+                }
+                return 1;
+            }
+            """
+    results = calculate_complexity(code)
+    assert ('a_function' in results)
+    assert (len(results) == 1)
+
+
+def test_a_function_which_calls_another_function_can_be_parsed():
+    code = """
+            int a_function (int a){
+                if (a > 5) {
+                    call_another_function(a + 1);
                 }
                 return 1;
             }
