8f344ee93d96a2925344bd0978c2c14153d9a882
Matt Chernosky <matt@chernosky.net>
2019-02-01 23:04:00 -0700

    refactor

--- ravioli/complexity.py
+++ ravioli/complexity.py
@@ -1,19 +1,30 @@
 import re
 
-decision_keywords = ['if', 'while', 'for']
+
+# Determine if this name is keyword that makes a decision/
+def is_a_decision(name):
+    decision_keywords = ['if', 'while', 'for']
+    return name in decision_keywords
+
+
+# Determine if this name is a function (and not a keyword that looks like one).
+def is_a_function(name):
+    keywords_that_look_like_functions = ['if', 'while', 'for', 'switch']
+    return name not in keywords_that_look_like_functions
+
 
 def calculate_complexity(code):
-    keywords = ['if', 'while', 'for', 'switch']
     results = {}
     function_matcher = re.compile(r'\s+(\w+)\s*\(.*\)\s*{', re.MULTILINE)
     current_function = ''
     for m in function_matcher.finditer(code):
         name = m.group(1)
-        if name not in keywords:
+        if is_a_function(name):
+            # We found a new function. Add it to the results.
             results[name] = 1
             # Save the current function.
             current_function = name
-        elif name in decision_keywords:
-            # This is a decision, increment the complexity.
+        elif is_a_decision(name):
+            # This is a decision, increment the complexity of the current function.
             results[current_function] += 1
     return results
