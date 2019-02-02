8e6f86e69c83a506f4ddf78619132f7ec5687c58
Matt Chernosky <matt@chernosky.net>
2019-02-01 23:58:14 -0700

    refactor

--- ravioli/complexity.py
+++ ravioli/complexity.py
@@ -13,6 +13,7 @@ def is_a_function(name):
     return name not in keywords_that_look_like_functions
 
 
+# Calculate the complexity of all the the functions in a string.
 def calculate_complexity(code):
     results = {}
     function_matcher = re.compile(r'\s+(\w+)\s*\(.*\)\s*{', re.MULTILINE)
@@ -20,25 +21,31 @@ def calculate_complexity(code):
     for m in function_matcher.finditer(code):
         name = m.group(1)
         if is_a_function(name):
-            # Find the contents of this function by matching braces.
-            start_index = m.end()
-            i = start_index
-            brace_nesting = 1
-            while brace_nesting > 0:
-                if code[i] == '{':
-                    brace_nesting += 1
-                elif code[i] == '}':
-                    brace_nesting -= 1
-                i += 1
-            end_index = i
-
+            start_of_function = m.end()
+            # We just found the next function. Extract the body of the function.
+            function_body = extract_next_function_body(code[start_of_function:])
             # Compute the complexity of this function.
-            results[name] = process_function_body(code[start_index:end_index])
+            results[name] = calculate_complexity_for_a_function(function_body)
 
     return results
 
 
-def process_function_body(body):
+# Extract the next function body in this section of code by matching braces.
+def extract_next_function_body(code):
+    i = 0
+    brace_nesting = 1
+    while brace_nesting > 0:
+        if code[i] == '{':
+            brace_nesting += 1
+        elif code[i] == '}':
+            brace_nesting -= 1
+        i += 1
+    end_index = i
+    return code[:end_index]
+
+
+# Calculate the complexity of a function.
+def calculate_complexity_for_a_function(body):
     complexity = 1
     for m in re.finditer(r'\s+(\w+)\s*\(.*\)', body, re.MULTILINE):
         name = m.group(1)
