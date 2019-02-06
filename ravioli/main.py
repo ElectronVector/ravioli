68e74895acb68f98b87035237003ad6475c01b08
Matt Chernosky <matt@chernosky.net>
2019-02-05 22:58:47 -0700

    Removed old c_parser

--- ravioli/main.py
+++ ravioli/main.py
@@ -1,9 +1,6 @@
 import sys
 from pathlib import Path
 
-from ravioli import c_parser
-from ravioli.line_counter import LineCounter
-
 if __name__ == "__main__":
     folder = Path('../motobox')
 
@@ -20,16 +17,16 @@ if __name__ == "__main__":
 
     print(f"Found {len(source_files)} source files...")
 
-    for f in source_files:
-        print(f"   {str(f)}")
-        try:
-            results = c_parser.parse(str(f), paths)
-            loc = LineCounter.count_file(f)
-            max_scc = max(results['complexity'].values())
-            sf = max_scc + (5*results['global_count']) + (loc // 20)
-            print(f"SLOC: {loc}, SCC: {max_scc}, Globals: {results['global_count']}, SF: {sf}")
-        except:
-            print("   Unable to parse")
+    # for f in source_files:
+    #     print(f"   {str(f)}")
+    #     try:
+    #         results = c_parser.parse(str(f), paths)
+    #         loc = LineCounter.count_file(f)
+    #         max_scc = max(results['complexity'].values())
+    #         sf = max_scc + (5*results['global_count']) + (loc // 20)
+    #         print(f"SLOC: {loc}, SCC: {max_scc}, Globals: {results['global_count']}, SF: {sf}")
+    #     except:
+    #         print("   Unable to parse")
 
     # with open("../motobox/Sources/can.c", 'r') as f:
     #     pprint(find_functions(f.read()))
