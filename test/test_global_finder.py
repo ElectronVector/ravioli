8566c1869ab24009f4cfea5524504620af805f55
Matt Chernosky <matt@chernosky.net>
2019-02-09 22:25:16 -0700

    Extract multiple globals

---
+++ test/test_global_finder.py
@@ -0,0 +1,28 @@
+import re
+
+
+def find_globals(code):
+    results = []
+    global_matcher = re.compile(r'\s+(\w+);')
+    for m in global_matcher.finditer(code):
+        name = m.group(1)
+        results.append(name)
+    return results
+
+
+def test_a_single_global():
+    code = """
+            int a_global;
+            """
+    results = find_globals(code)
+    assert ('a_global' in results)
+
+
+def test_multiple_globals():
+    code = """
+            int a_global;
+            uint8_t another_global;
+            """
+    results = find_globals(code)
+    assert ('a_global' in results)
+    assert ('another_global' in results)
