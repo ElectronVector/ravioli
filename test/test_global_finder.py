bad4c543292c726e57ec7699accf6e3549188a55
Matt Chernosky <matt@chernosky.net>
2019-02-09 23:06:56 -0700

    Assignment with spaces

--- test/test_global_finder.py
+++ test/test_global_finder.py
@@ -27,8 +27,18 @@ def test_a_global_with_assignment():
     assert ('a_global' in results)
 
 
+def test_with_assignment_and_whitespace():
+    code = """
+            int a_global = 0;
+            """
+    results = find_globals(code)
+    assert ('a_global' in results)
+
+
 def find_globals(code):
     results = []
+    # Remove whitespace around any equals.
+    code = re.sub(r'\s*=\s*', '=', code)
     global_matcher = re.compile(r'\s+(\w+)[;|=]')
     for m in global_matcher.finditer(code):
         name = m.group(1)
