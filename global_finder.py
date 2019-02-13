ecd134521baa4565afde32b665891f3271271f00
Matt Chernosky <matt@chernosky.net>
2019-02-12 23:44:53 -0700

    Cleanup

--- global_finder.py
+++ global_finder.py
@@ -12,6 +12,8 @@ def find_globals(code):
     for m in global_matcher.finditer(code):
         qualifiers = m.group(1)
         name = m.group(2)
-        if 'static' not in qualifiers and 'typedef' not in qualifiers and 'extern' not in qualifiers:
+        if ('static' not in qualifiers
+                and 'typedef' not in qualifiers
+                and 'extern' not in qualifiers):
             results.append(name)
     return results
