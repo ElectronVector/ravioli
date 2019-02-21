c9dd12398b942681353ac457d92360994b6b7340
Matt Chernosky <matt@chernosky.net>
2019-02-21 08:16:42 -0700

    Fix block comment stripping

--- ravioli/strip_comments.py
+++ ravioli/strip_comments.py
@@ -1,10 +1,16 @@
-import re
-
-
 def strip_comments(code):
-    # Remove block comments
-    code = re.sub(r'/\*.*\*/', '', code, flags=re.DOTALL)
     # Remove single line comments.
     code_lines = code.splitlines(True)
     non_comment_lines = [line for line in code_lines if not line.lstrip().startswith("//")]
-    return "".join(non_comment_lines)
+    code = "".join(non_comment_lines)
+    # Remove block comments
+    stripped = ""
+    in_comment = False
+    for i, c in enumerate(code):
+        if code[i] == '/' and code[i + 1] == '*':
+            in_comment = True
+        if not in_comment:
+            stripped += code[i]
+        elif code[i - 1] == '*' and code[i] == '/':
+            in_comment = False
+    return stripped
--- test/test_strip_comments.py
+++ test/test_strip_comments.py
@@ -1,5 +1,6 @@
 from ravioli.strip_comments import strip_comments
 
+
 def test_multiline_block_comment():
     code = """
             /* See if the handle of the queue being unregistered in actually in the
@@ -13,56 +14,68 @@ def test_multiline_block_comment():
     stripped = strip_comments(code)
     assert(stripped == expected)
 
-# def test_some_tricky_code():
-#     code = """
-#                 /*-----------------------------------------------------------*/
-#
-#             #if configQUEUE_REGISTRY_SIZE > 0
-#
-#                 static void vQueueUnregisterQueue( xQueueHandle xQueue )
-#                 {
-#                 unsigned portBASE_TYPE ux;
-#
-#                     /* See if the handle of the queue being unregistered in actually in the
-#                     registry. */
-#                     for( ux = 0; ux < configQUEUE_REGISTRY_SIZE; ux++ )
-#                     {
-#                         if( xQueueRegistry[ ux ].xHandle == xQueue )
-#                         {
-#                             /* Set the name to NULL to show that this slot if free again. */
-#                             xQueueRegistry[ ux ].pcQueueName = NULL;
-#                             break;
-#                         }
-#                     }
-#
-#                 }
-#
-#             #endif
-#             """
-#     expected = """
-#
-#
-#             #if configQUEUE_REGISTRY_SIZE > 0
-#
-#                 static void vQueueUnregisterQueue( xQueueHandle xQueue )
-#                 {
-#                 unsigned portBASE_TYPE ux;
-#
-#
-#                     for( ux = 0; ux < configQUEUE_REGISTRY_SIZE; ux++ )
-#                     {
-#                         if( xQueueRegistry[ ux ].xHandle == xQueue )
-#                         {
-#
-#                             xQueueRegistry[ ux ].pcQueueName = NULL;
-#                             break;
-#                         }
-#                     }
-#
-#                 }
-#
-#             #endif
-#             """
-#
-#     stripped = strip_comments(code)
-#     assert(stripped == expected)
+
+def test_dashed_comment_line():
+    code = """
+            /*-----------------------------------------------------------*/
+            int a;
+            """
+    expected = """
+            
+            int a;
+            """
+    stripped = strip_comments(code)
+    assert(stripped == expected)
+
+
+def test_some_tricky_code():
+    code = """
+                /*-----------------------------------------------------------*/
+            
+            #if configQUEUE_REGISTRY_SIZE > 0
+
+                static void vQueueUnregisterQueue( xQueueHandle xQueue )
+                {
+                unsigned portBASE_TYPE ux;
+
+                    /* See if the handle of the queue being unregistered in actually in the
+                    registry. */
+                    for( ux = 0; ux < configQUEUE_REGISTRY_SIZE; ux++ )
+                    {
+                        if( xQueueRegistry[ ux ].xHandle == xQueue )
+                        {
+                            /* Set the name to NULL to show that this slot if free again. */
+                            xQueueRegistry[ ux ].pcQueueName = NULL;
+                            break;
+                        }
+                    }
+                }
+
+            #endif
+            """
+    expected = """
+                
+            
+            #if configQUEUE_REGISTRY_SIZE > 0
+
+                static void vQueueUnregisterQueue( xQueueHandle xQueue )
+                {
+                unsigned portBASE_TYPE ux;
+
+                    
+                    for( ux = 0; ux < configQUEUE_REGISTRY_SIZE; ux++ )
+                    {
+                        if( xQueueRegistry[ ux ].xHandle == xQueue )
+                        {
+                            
+                            xQueueRegistry[ ux ].pcQueueName = NULL;
+                            break;
+                        }
+                    }
+                }
+
+            #endif
+            """
+
+    stripped = strip_comments(code)
+    assert(stripped == expected)
