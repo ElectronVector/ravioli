9b793f431c48fc31c44cb3d5a1198c186a151fa8
Matt Chernosky <matt@chernosky.net>
2019-02-23 23:48:43 -0700

    Add -f option for printing all globals and functions
    Sort all functions by complexity

--- README.md
+++ README.md
@@ -5,10 +5,49 @@ A tool for calculating simple, useful complexity metrics for C.
 This tool is designed to work especially on embedded software written for compilers with non-standard extensions.
 It works without a compiler or any preprocessing required.
 
+## Usage
+
+You can run this on a single file and it will compute metrics for it. Pass it a folder however and it will calculate metrics on all c files it finds in there.
+
+Use it in the current folder like this:
+
+```
+$ ravioli .
+```
+
+And you'll get a list of all modules sorted by Koopman Spaghetti Factor (KSF):
+
+```
+-------------------------------------------------------------------------------
+File                                         complexity   globals   lines   ksf
+-------------------------------------------------------------------------------
+motobox\Sources\FreeRTOS\tasks.c                     12         0    1387    81
+motobox\Sources\datapage.c                            1         0    1242    63
+motobox\Sources\FreeRTOS\queue.c                     15         0     930    61
+motobox\Sources\command_processor.c                  19         2     243    41
+motobox\Sources\rtos.c                                5         6     135    41
+motobox\Sources\vehicle_comm.c                        8         1     432    34
+motobox\Sources\vehicle_comm_sim.c                   11         0     373    29
+motobox\Sources\Start12.c                             1         1     337    22
+motobox\Sources\can.c                                 7         0     289    21
+motobox\Sources\iso15765.c                           12         0     187    21
+motobox\Sources\flash.c                               7         0     268    20
+motobox\Sources\j1979.c                              10         0     201    20
+motobox\Sources\Cpu.C                                 2         2      40    14
+motobox\Sources\leds.c                                2         2      26    13
+motobox\Sources\log.c                                 3         1     117    13
+motobox\Sources\rti.c                                 2         2      23    13
+```
+
+There is also a `-f` flag that you can select, that will list all global variables and all functions, sorted by complexity:
+
+```
+$ ravioli -f .
+```
+
 ## To do
 
-- Provide coordinates for functions and globals.
+- Clean up output format on -f option. Only provide filename, not full path?
+- Provide coordinates for functions and globals when using the -f option.
 - Add better error handling. If the parser has an error, make it easy to find the code that broke it.
 - Try it on some other code.
-- Run it on a single file or on a folder?
-- Make the output cleaner.
--- ravioli/run.py
+++ ravioli/run.py
@@ -3,19 +3,58 @@ import os
 import sys
 import traceback
 from operator import itemgetter
-
 from pathlib import Path
-from pprint import pprint
 
 from ravioli.complexity import calculate_complexity
 from ravioli.global_finder import find_globals
 from ravioli.line_counter import count
 
 
-def run(filename):
+def run(filename, full_format):
+    if full_format:
+        report_all_functions(filename)
+    else:
+        report_ksf_for_all_modules(filename)
+
+
+def report_all_functions(filename):
+    results = []
+    if not os.path.isdir(filename):
+        # This is a single file.
+        results.append(run_single_file(filename))
+    else:
+        # This is a directory. Run on all the files we can find.
+        source_files = list(Path(filename).glob('**/*.c'))
+
+        for f in source_files:
+            results.append(run_single_file(str(f)))
+
+    # Print globals.
     print("-------------------------------------------------------------------------------")
-    print("File                                         complexity   globals   lines   ksf")
+    print("Globals                                                                        ")
     print("-------------------------------------------------------------------------------")
+    for result in results:
+        for g in result['globals_vars']:
+            print(result['filename'] + ": " + g)
+
+    # Assemble all the functions into one list.
+    functions = []
+    for result in results:
+        for name, complexity in result['functions'].items():
+            functions.append({'filename': result['filename'], 'name': name, 'complexity': complexity})
+
+    # Sort the functions by complexity.
+    functions = sorted(functions, key=itemgetter('complexity'), reverse=True)
+
+    # Print functions.
+    print("-------------------------------------------------------------------------------")
+    print("Functions                                                                      ")
+    print("-------------------------------------------------------------------------------")
+    for f in functions:
+        print(str(f['complexity']) + " " + f['name'] + " " + f['filename'])
+
+
+def report_ksf_for_all_modules(filename):
     results = []
     if not os.path.isdir(filename):
         # This is a single file.
@@ -30,14 +69,19 @@ def run(filename):
     # Sort by spaghetti factor.
     results = sorted(results, key=itemgetter('ksf'), reverse=True)
 
+    print("-------------------------------------------------------------------------------")
+    print("File                                         complexity   globals   lines   ksf")
+    print("-------------------------------------------------------------------------------")
     for result in results:
         if len(result['filename']) <= 50:
             print("{file:50}  {complexity:3}       {globals:3}   {loc:5}   {ksf:3}".format(
-                file=result['filename'], complexity=result['max_scc'], globals=len(result['globals_vars']), loc=result['loc'], ksf=result['ksf']))
+                file=result['filename'], complexity=result['max_scc'], globals=len(result['globals_vars']),
+                loc=result['loc'], ksf=result['ksf']))
         else:
             print(result['filename'])
             print("{placeholder:50}  {complexity:3}       {globals:3}   {loc:5}   {ksf:3}".format(
-                placeholder="", complexity=result['max_scc'], globals=len(result['globals_vars']), loc=result['loc'], ksf=result['ksf']))
+                placeholder="", complexity=result['max_scc'], globals=len(result['globals_vars']), loc=result['loc'],
+                ksf=result['ksf']))
 
 
 def run_single_file(filename):
@@ -51,7 +95,8 @@ def run_single_file(filename):
             max_scc = find_max_complexity(functions)
             # Calculate the spaghetti factor.
             ksf = max_scc + (5 * len(globals_vars)) + (loc // 20)
-            return {'filename': filename, 'max_scc': max_scc, 'globals_vars': globals_vars, 'loc': loc, 'ksf': ksf}
+            return {'filename': filename, 'functions': functions, 'max_scc': max_scc, 'globals_vars': globals_vars,
+                    'loc': loc, 'ksf': ksf}
     except:
         print('*** unable to parse ({file})'.format(file=filename))
         traceback.print_exc(file=sys.stdout)
@@ -66,8 +111,9 @@ def find_max_complexity(functions):
 
 
 parser = argparse.ArgumentParser(description='Calculate complexity metrics for C code, specifically the Koopman '
-                                             'Spaghetti Factor.')
+                                             'Spaghetti Factor (KSF).')
 parser.add_argument('source', help='the source file or folder for which to calculate metrics')
+parser.add_argument('-f', action='store_true', help='output a complete list of all globals and functions sorted by complexity')
 args = parser.parse_args()
-run(args.source)
+run(args.source, args.f)
 
