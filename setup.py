8f8ea3fc3c13454c7b48273b5d7d7c65f2272eca
Matt Chernosky <matt@chernosky.net>
2019-03-03 20:42:12 -0700

    Add long description to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 setup(name='ravioli',
       version='0.1.0',
       description='Checks for spaghetti code',
+      long_description='A tool for calculating simple, useful complexity metrics -- notably the Koopman Spaghetti '
+                       'Factor (KSF) -- for C. This tool is designed to work especially on embedded software written '
+                       'for compilers with non-standard extensions. It works without a compiler or any preprocessing '
+                       'required.',
       url='https://github.com/ElectronVector/ravioli',
       author='Matt Chernosky',
       author_email='matt@electronvector.com',
