9ed946afd9b08eedcf284b603c6221ed17f4476b
mchernosky <matt@chernosky.net>
2019-09-27 20:50:35 -0600

    Test for globals

--- test/test_command_line.py
+++ test/test_command_line.py
@@ -3,17 +3,26 @@ import sys
 from ravioli.ravioli import main
 
 
-def run_and_capture_stdout():
-    so = sys.stdout
-    capture_stdout = io.StringIO()
-    sys.stdout = capture_stdout
+def run_ravioli():
+    """ Run ravioli and capture stdout.
+    :return: The stdout captured during execution.
+    """
+    original_stdout = sys.stdout
+    captured_stdout = io.StringIO()
+    sys.stdout = captured_stdout
     main()
-    sys.stdout = so
-    return capture_stdout.getvalue()
+    sys.stdout = original_stdout
+    return captured_stdout.getvalue()
 
 
 def test_results_for_files():
     sys.argv = ["ravioli", "."]
-    stdout = run_and_capture_stdout()
+    stdout = run_ravioli()
     assert ("sample.c" in stdout)
     assert ("main.c" in stdout)
+
+def test_f_option_includes_globals():
+    sys.argv = ["ravioli", ".", "-f"]
+    stdout = run_ravioli()
+    assert ("sample.c:2 global_variable" in stdout)
+    assert ("sample.c:3 another_global" in stdout)
