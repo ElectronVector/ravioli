be68439988c023c2dde76c9a8694530f4d2a3df1
mchernosky <matt@chernosky.net>
2021-08-20 17:02:45 -0600

    Don't parse the internal struct members as variable declarations

--- ravioli/global_processor.py
+++ ravioli/global_processor.py
@@ -8,18 +8,21 @@ from pyparsing import (
     Optional,
     delimitedList,
     Group,
-    Keyword, Char, SkipTo,
+    Keyword, Char, SkipTo, nestedExpr,
 )
 
 
 def find_variables(code):
+
     type_ = Word(alphanums + "_")
     name = Word(alphas, alphanums + "_")
     assignment = Optional(Char("=") + SkipTo(oneOf(", ;")))
     variable_declaration = type_("type")\
         + delimitedList(name("name") + assignment)\
         + ";"
+    struct_definition = Keyword("struct") + nestedExpr("{", "}") + name("name")
     variables = []
-    for var, start, end in variable_declaration.scanString(code):
+    for var, start, end in (variable_declaration | struct_definition).scanString(code):
         variables.append(var.name)
+    print(variables)
     return variables
