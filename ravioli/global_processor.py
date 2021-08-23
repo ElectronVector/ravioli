a83682c39b65bb6a50fa59006ae70b5613dde29c
mchernosky <matt@chernosky.net>
2021-08-23 12:26:31 -0600

    Refactor

--- ravioli/global_processor.py
+++ ravioli/global_processor.py
@@ -17,25 +17,25 @@ from ravioli.variable import Variable
 def find_variables(code):
 
     type_ = Word(alphanums + "_")
-    indentifier = Word(alphas, alphanums + "_")
+    identifier = Word(alphas, alphanums + "_")
     assignment = Char("=") + Word(printables + " ", excludeChars=",;")
     block = nestedExpr("{", "}")
     array = "[" + Word(alphanums + "_") + "]"
 
-    decl_single = indentifier("name") + Optional(assignment)
-    decl_array = indentifier("name") + array
-    decl_array_with_assignment = indentifier("name") + array + "=" + block
+    decl_single = identifier("name") + Optional(assignment)
+    decl_array = identifier("name") + array
+    decl_array_with_assignment = identifier("name") + array + "=" + block
 
     variable_declaration = type_("type")\
         + delimitedList(decl_array_with_assignment | decl_array | decl_single)\
         + ";"
 
-    struct = Keyword("struct") + Optional(indentifier) + block
-    struct_definition = struct + Optional(indentifier("name")) + ";"
+    struct = Keyword("struct") + Optional(identifier) + block
+    struct_definition = struct + Optional(identifier("name")) + ";"
     struct_typedef = Keyword("typedef") + struct + Optional(type_) + ";"
 
     sign = Keyword("unsigned") | Keyword("signed")
-    typedef = Keyword("typedef") + Optional(sign) + type_ + indentifier + ";"
+    typedef = Keyword("typedef") + Optional(sign) + type_ + identifier + ";"
 
     statements = [
         typedef,
