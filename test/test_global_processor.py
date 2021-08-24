93340878a7514aa008615e927cf217ecab683358
mchernosky <matt@chernosky.net>
2021-08-24 09:05:00 -0600

    Organize tests with some classes

--- test/test_global_processor.py
+++ test/test_global_processor.py
@@ -64,6 +64,7 @@ def test_find_a_compound_type_variable_declaration():
     variables = find_variables(code)
     assert (Variable("a_variable") in variables)
 
+
 def test_find_multiple_variables_in_the_same_line():
     code = """
                 int a, b;
@@ -120,206 +121,189 @@ def test_do_not_find_nondeclaration_assignment_from_function_call():
     assert (Variable("a") not in variables)
 
 
-# Structs
+class TestStucts:
 
-def test_do_not_find_struct_members():
-    code = """
-            struct my_struct {
-                int a;
-                int b;
-                int c;
-            };
-            """
-    variables = find_variables(code)
-    assert (Variable("a") and Variable("b") and Variable("c") not in variables)
+    def test_do_not_find_struct_members(self):
+        code = """
+                struct my_struct {
+                    int a;
+                    int b;
+                    int c;
+                };
+                """
+        variables = find_variables(code)
+        assert (Variable("a") and Variable("b") and Variable("c") not in variables)
 
+    def test_do_not_find_anonymous_struct_members(self):
+        code = """
+                struct {
+                    int a;
+                    int b;
+                    int c;
+                } my_struct;
+                """
+        variables = find_variables(code)
+        assert (Variable("a") and Variable("b") and Variable("c") not in variables)
 
-def test_do_not_find_anonymous_struct_members():
-    code = """
-            struct {
-                int a;
-                int b;
-                int c;
-            } my_struct;
-            """
-    variables = find_variables(code)
-    assert (Variable("a") and Variable("b") and Variable("c") not in variables)
+    def test_find_anonymous_struct_declaration(self):
+        code = """
+                struct {
+                    int a;
+                    int b;
+                    int c;
+                } my_struct;
+                """
+        variables = find_variables(code)
+        assert (Variable("my_struct") in variables)
 
+    def test_find_anonymous_struct_array_declaration(self):
+        code = """
+                struct {
+                    int a;
+                    int b;
+                    int c;
+                } my_struct[10];
+                """
+        variables = find_variables(code)
+        assert (Variable("my_struct") in variables)
 
-def test_find_anonymous_struct_declaration():
-    code = """
-            struct {
-                int a;
-                int b;
-                int c;
-            } my_struct;
-            """
-    variables = find_variables(code)
-    assert (Variable("my_struct") in variables)
+    def test_find_named_struct_array_declaration(self):
+        code = """
+                struct a_struct {
+                    int a;
+                    int b;
+                    int c;
+                } my_struct[10];
+                """
+        variables = find_variables(code)
+        assert (Variable("my_struct") in variables)
 
+    def test_do_not_find_typedef_struct_members(self):
+        code = """
+                typedef struct {
+                    int a;
+                    int b;
+                    int c;
+                } my_struct_t;
+                """
+        variables = find_variables(code)
+        assert (Variable("a") and Variable("b") and Variable("c") not in variables)
 
-def test_find_anonymous_struct_array_declaration():
-    code = """
-            struct {
-                int a;
-                int b;
-                int c;
-            } my_struct[10];
-            """
-    variables = find_variables(code)
-    assert (Variable("my_struct") in variables)
+    def test_do_not_find_struct_typedef_name(self):
+        code = """
+                typedef struct {
+                    int a;
+                    int b;
+                    int c;
+                } my_struct_t;
+                """
+        variables = find_variables(code)
+        assert (not variables)
 
+    def test_do_not_find_named_struct_with_typedef_name(self):
+        code = """
+                typedef struct my_struct {
+                    int a;
+                    int b;
+                    int c;
+                } my_struct_t;
+                """
+        variables = find_variables(code)
+        assert (not variables)
 
-def test_find_named_struct_array_declaration():
-    code = """
-            struct a_struct {
-                int a;
-                int b;
-                int c;
-            } my_struct[10];
-            """
-    variables = find_variables(code)
-    assert (Variable("my_struct") in variables)
+    def test_find_struct_delcared_with_defintion(self):
+        code = """
+                struct my_struct {
+                    int a;
+                    int b;
+                    int c;
+                } a;
+                """
+        variables = find_variables(code)
+        assert (variables == [Variable("a")])
 
-
-def test_do_not_find_typedef_struct_members():
-    code = """
-            typedef struct {
-                int a;
-                int b;
-                int c;
-            } my_struct_t;
-            """
-    variables = find_variables(code)
-    assert (Variable("a") and Variable("b") and Variable("c") not in variables)
-
-
-def test_do_not_find_struct_typedef_name():
-    code = """
-            typedef struct {
-                int a;
-                int b;
-                int c;
-            } my_struct_t;
-            """
-    variables = find_variables(code)
-    assert (not variables)
-
-
-def test_do_not_find_named_struct_with_typedef_name():
-    code = """
-            typedef struct my_struct {
-                int a;
-                int b;
-                int c;
-            } my_struct_t;
-            """
-    variables = find_variables(code)
-    assert (not variables)
-
-
-def test_find_struct_delcared_with_defintion():
-    code = """
-            struct my_struct {
-                int a;
-                int b;
-                int c;
-            } a;
-            """
-    variables = find_variables(code)
-    assert (variables == [Variable("a")])
-
-
-def test_find_struct_delcared_without_defintion():
-    code = """
-            struct my_struct_t a;
-            """
-    variables = find_variables(code)
-    assert (variables == [Variable("a")])
+    def test_find_struct_delcared_without_defintion(self):
+        code = """
+                struct my_struct_t a;
+                """
+        variables = find_variables(code)
+        assert (variables == [Variable("a")])
 
 
-# Typedefs
+class TestTypedefs:
 
-def test_typedefs_not_found():
-    code = """
-            typedef int my_int_t;
-            """
-    found = find_variables(code)
-    assert (Variable("my_int_t") not in found)
+    def test_typedefs_not_found(self):
+        code = """
+                typedef int my_int_t;
+                """
+        found = find_variables(code)
+        assert (Variable("my_int_t") not in found)
 
+    def test_unsigned_typedefs_not_found(self):
+        code = """
+                typedef unsigned int my_int_t;
+                """
+        found = find_variables(code)
+        assert (Variable("my_int_t") not in found)
 
-def test_unsigned_typedefs_not_found():
-    code = """
-            typedef unsigned int my_int_t;
-            """
-    found = find_variables(code)
-    assert (Variable("my_int_t") not in found)
+    def test_signed_typedefs_not_found(self):
+        code = """
+                typedef signed int my_int_t;
+                """
+        found = find_variables(code)
+        assert (Variable("my_int_t") not in found)
 
-
-def test_signed_typedefs_not_found():
-    code = """
-            typedef signed int my_int_t;
-            """
-    found = find_variables(code)
-    assert (Variable("my_int_t") not in found)
-
-
-def test_typedef_array_not_found():
-    code = """
-            typedef int my_array_t[10];
-            """
-    found = find_variables(code)
-    assert (Variable("my_array_t") not in found)
+    def test_typedef_array_not_found(self):
+        code = """
+                typedef int my_array_t[10];
+                """
+        found = find_variables(code)
+        assert (Variable("my_array_t") not in found)
 
 
-# Arrays
+class TestArrays:
 
-def test_array_found():
-    code = """
-            char a[10];
-            """
-    found = find_variables(code)
-    assert (Variable("a") in found)
+    def test_array_found(self):
+        code = """
+                char a[10];
+                """
+        found = find_variables(code)
+        assert (Variable("a") in found)
 
+    def test_array_with_non_numeric_size_arg_found(self):
+        code = """
+                char a[MAX_SIZE];
+                """
+        found = find_variables(code)
+        assert (Variable("a") in found)
 
-def test_array_with_non_numeric_size_arg_found():
-    code = """
-            char a[MAX_SIZE];
-            """
-    found = find_variables(code)
-    assert (Variable("a") in found)
+    def test_array_with_initializer_found(self):
+        code = """
+                char a[MAX_SIZE] = {1, 2, 3};
+                """
+        found = find_variables(code)
+        assert (Variable("a") in found)
 
+    def test_unsized_array_with_initializer_found(self):
+        code = """
+                char a[] = {1, 2, 3};
+                """
+        found = find_variables(code)
+        assert (Variable("a") in found)
 
-def test_array_with_initializer_found():
-    code = """
-            char a[MAX_SIZE] = {1, 2, 3};
-            """
-    found = find_variables(code)
-    assert (Variable("a") in found)
+    def test_mutliple_arrays_at_once(self):
+        code = """
+                char a[3], b[10];
+                """
+        found = find_variables(code)
+        assert (Variable("a") and Variable("b") in found)
 
-
-def test_unsized_array_with_initializer_found():
-    code = """
-            char a[] = {1, 2, 3};
-            """
-    found = find_variables(code)
-    assert (Variable("a") in found)
-
-
-def test_mutliple_arrays_at_once():
-    code = """
-            char a[3], b[10];
-            """
-    found = find_variables(code)
-    assert (Variable("a") and Variable("b") in found)
-
-
-def test_mutliple_arrays_at_once_with_initializer():
-    code = """
-            char a[3] = {1, 2, 3}, b[10];
-            """
-    found = find_variables(code)
-    assert (Variable("a") and Variable("b") in found)
+    def test_mutliple_arrays_at_once_with_initializer(self):
+        code = """
+                char a[3] = {1, 2, 3}, b[10];
+                """
+        found = find_variables(code)
+        assert (Variable("a") and Variable("b") in found)
 
 
 def test_find_line_number():
