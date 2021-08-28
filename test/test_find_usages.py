4978c13eb18d427edab12e5a7f8de8641a9a5e2f
mchernosky <matt@chernosky.net>
2021-08-28 07:57:08 -0600

    Add next failling test

--- test/test_find_usages.py
+++ test/test_find_usages.py
@@ -15,3 +15,12 @@ def test_find_an_assignment_from_variable():
             """
     u = find_usages(code)
     assert ("a_variable" and "another_variable" in u)
+
+
+def test_dont_find_a_declaration():
+    code = """
+            int a_declaration = another_variable;
+            """
+    u = find_usages(code)
+    assert ("a_declaration" not in u)
+    assert ("another_variable" in u)
