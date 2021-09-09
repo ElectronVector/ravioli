29b0bbfb66152fbfb1de2507091b4b63b81f90ec
Matt <matt@electronvector.com>
2021-09-08 23:11:30 -0600

    Refactor extractor

--- ravioli/extract_variables.py
+++ ravioli/extract_variables.py
@@ -16,26 +16,23 @@ class Token:
 
 class VariableExtractor:
     def __init__(self):
-        self.variables = {
-            "declarations": [],
-            "usages": [],
-            "functions": []
-        }
+        self.declarations = []
+        self.usages = []
+        self.functions = []
 
     def extract_declaration(self, token):
-        self.variables["declarations"].append(Token(token[1]))
+        self.declarations.append(Token(token[1]))
         print(f"extracting declaration: {token}")
 
     def extract_assignment(self, token):
-        self.variables["usages"].append(Token(token[0]))
+        self.usages.append(Token(token[0]))
         print(f"extracting assignment: {token}")
 
     def extract_function(self, token):
-        self.variables["functions"].append(Token(token[1]))
+        self.functions.append(Token(token[1]))
         print(f"extracting function: {token}")
 
     def extract(self, code):
-
         type_ = Word(alphanums)
         variable_name = Word(alphas, alphanums + "_")
         declaration = type_ + variable_name + ";"
@@ -49,8 +46,16 @@ class VariableExtractor:
 
         ZeroOrMore(MatchFirst([assignment, declaration, function])).parseString(code)
 
-        return self.variables
+        return self
+
+    # Get all of the extracted stuff in a single dictionary.
+    def get_all_as_dict(self):
+        return {
+            "declarations": self.declarations,
+            "usages": self.usages,
+            "functions": self.functions,
+        }
 
 
 def extract_variables(code):
-    return VariableExtractor().extract(code)
+    return VariableExtractor().extract(code).get_all_as_dict()
