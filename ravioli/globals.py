c587cf3914199cfcda4a775989aac8e8dc08dcac
Matt <matt@electronvector.com>
2021-09-14 15:23:44 -0600

    Refactor

--- ravioli/globals.py
+++ ravioli/globals.py
@@ -19,15 +19,21 @@ def clean_up_whitespace(s):
     return " ".join(s.strip().split())
 
 
+def extract_definitions_from_statement(statement):
+    tokens = statement.split()
+    if tokens[0].isalpha() and tokens[1].isalpha():
+        # This is a declaration. Extract the second token.
+        return tokens[1]
+
+
 def extract_undefined_usages(code):
     statements = extract_statements(code)
     declarations = []
     usages = []
     for s in statements:
-        tokens = s.split()
-        if tokens[0].isalpha() and tokens[1].isalpha():
-            # This is a declaration. Extract the second token.
-            declarations.append(tokens[1])
+        new_declarations = extract_definitions_from_statement(s)
+        if new_declarations:
+            declarations += new_declarations
         else:
             usages += [t for t in s.split() if t.isalpha()]
 
