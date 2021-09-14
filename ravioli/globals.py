d05904e1e359fef597cb5807c7a90118defd24ce
Matt <matt@electronvector.com>
2021-09-14 15:16:06 -0600

    Try finding some definitions and usages

--- ravioli/globals.py
+++ ravioli/globals.py
@@ -17,3 +17,20 @@ def extract_statements(code):
 
 def clean_up_whitespace(s):
     return " ".join(s.strip().split())
+
+
+def extract_undefined_usages(code):
+    statements = extract_statements(code)
+    declarations = []
+    usages = []
+    for s in statements:
+        tokens = s.split()
+        if tokens[0].isalpha() and tokens[1].isalpha():
+            # This is a declaration. Extract the second token.
+            declarations.append(tokens[1])
+        else:
+            usages += [t for t in s.split() if t.isalpha()]
+
+    print(usages)
+    print(declarations)
+    return [u for u in usages if u not in declarations]
--- test/test_globals.py
+++ test/test_globals.py
@@ -1,4 +1,4 @@
-from ravioli.globals import extract_statements
+from ravioli.globals import extract_statements, extract_undefined_usages
 
 
 def test_single_statement():
@@ -23,3 +23,29 @@ def test_multiple_statements_with_extra_whitespace():
             c;
     """
     assert extract_statements(code) == ["int a", "int b = 0", "int c"]
+
+
+
+
+
+def test_do_not_find_a_defined_usage():
+    code = """
+            int a;
+            a = 0;
+        """
+    assert extract_undefined_usages(code) == []
+
+
+def test_find_an_undefined_usage():
+    code = """
+            a = 0;
+        """
+    assert extract_undefined_usages(code) == ["a"]
+
+
+def test_do_not_find_an_undefined_usage_after_definition_and_assignment():
+    code = """
+            int a = 0;
+            a = 1;
+        """
+    assert extract_undefined_usages(code) == []
