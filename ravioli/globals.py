b968d2e30e1769bc2c7e329ae1b275506dad8137
Matt <matt@electronvector.com>
2021-09-16 13:33:03 -0600

    Rename

--- ravioli/globals.py
+++ ravioli/globals.py
@@ -25,17 +25,17 @@ def extract_declarations_from_statement(statement):
     # Split the statement into tokens by spaces.
     tokens = statement.split()
     print(tokens)
-    decl = None
+    potential_declaration = None
     declarations = []
     on_right_side_of_equals = False
     # Iterate over the list, looking for two or more identifiers next to each other from the start.
     for i, t in enumerate(tokens):
         if t == ",":
             # A comma indicates the end of a declaration and that their will be another. Save this one and don't stop
-            # yet.
-            if decl:
-                declarations.append(decl)
-                decl = None
+            # yet. If we have previously saved a declaration, append it to our list.
+            if potential_declaration:
+                declarations.append(potential_declaration)
+                potential_declaration = None
 
             # Once we get to a comma, we are back on the left side of the equals sign.
             on_right_side_of_equals = False
@@ -43,11 +43,11 @@ def extract_declarations_from_statement(statement):
             # We can't find an assignment on the right side of an equals sign.
             on_right_side_of_equals = True
         elif i > 0 and is_valid_identifier(t) and not on_right_side_of_equals:
-            # Save the last valid identifier because this is going to be the declaration.
-            decl = t
+            # Save the last valid identifier because this is could be the declaration (if it's actually the last one.
+            potential_declaration = t
 
-    if decl:
-        declarations.append(decl)
+    if potential_declaration:
+        declarations.append(potential_declaration)
 
     if declarations:
         return declarations
