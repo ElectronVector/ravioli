edb91c6110f6624e19fd8b5854209c6d210809d8
Matt <matt@electronvector.com>
2021-09-16 13:29:57 -0600

    Extract compound declarations with commas

--- ravioli/globals.py
+++ ravioli/globals.py
@@ -20,20 +20,37 @@ def clean_up_whitespace(s):
 
 
 def extract_declarations_from_statement(statement):
+    # Ensure that there is whitespace around all commas.
+    statement = statement.replace(",", " , ")
     # Split the statement into tokens by spaces.
     tokens = statement.split()
     print(tokens)
-    declaration = None
+    decl = None
+    declarations = []
+    on_right_side_of_equals = False
     # Iterate over the list, looking for two or more identifiers next to each other from the start.
     for i, t in enumerate(tokens):
-        if not is_valid_identifier(t):
-            break
-        elif i > 0:
-            # Save the last valid identifier because this is going to be the declaration.
-            declaration = t
+        if t == ",":
+            # A comma indicates the end of a declaration and that their will be another. Save this one and don't stop
+            # yet.
+            if decl:
+                declarations.append(decl)
+                decl = None
 
-    if declaration:
-        return [declaration]
+            # Once we get to a comma, we are back on the left side of the equals sign.
+            on_right_side_of_equals = False
+        elif t == "=":
+            # We can't find an assignment on the right side of an equals sign.
+            on_right_side_of_equals = True
+        elif i > 0 and is_valid_identifier(t) and not on_right_side_of_equals:
+            # Save the last valid identifier because this is going to be the declaration.
+            decl = t
+
+    if decl:
+        declarations.append(decl)
+
+    if declarations:
+        return declarations
 
 
 def is_valid_identifier(s):
