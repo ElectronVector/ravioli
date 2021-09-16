0d10a7f984ff7a0ef55434237a37d5d6c3b13f79
Matt <matt@electronvector.com>
2021-09-16 13:53:49 -0600

    Handle things that look like compound declarations but aren't

--- ravioli/globals.py
+++ ravioli/globals.py
@@ -24,33 +24,38 @@ def extract_declarations_from_statement(statement):
     statement = statement.replace(",", " , ")
     # Split the statement into tokens by spaces.
     tokens = statement.split()
-    print(tokens)
-    potential_declaration = None
     declarations = []
+
+    type_found = None
+    potential_declaration = []
     on_right_side_of_equals = False
     # Iterate over the list, looking for two or more identifiers next to each other from the start.
     for i, t in enumerate(tokens):
         if t == ",":
             # A comma indicates the end of a declaration and that their will be another. Save this one and don't stop
-            # yet. If we have previously saved a declaration, append it to our list.
-            if potential_declaration:
-                declarations.append(potential_declaration)
-                potential_declaration = None
-
+            # yet. If we have previously saved a declaration with at least two consecutive identifiers, save it.
+            if len(potential_declaration) >= 2:
+                # The last identifier saved is the name of the declaration.
+                declarations.append(potential_declaration[-1])
+                type_found = True
+            potential_declaration = []
             # Once we get to a comma, we are back on the left side of the equals sign.
             on_right_side_of_equals = False
         elif t == "=":
             # We can't find an assignment on the right side of an equals sign.
             on_right_side_of_equals = True
-        elif i > 0 and is_valid_identifier(t) and not on_right_side_of_equals:
-            # Save the last valid identifier because this is could be the declaration (if it's actually the last one.
-            potential_declaration = t
+        elif is_valid_identifier(t) and not on_right_side_of_equals:
+            # Save all the valid consecutive identifier so that we can eventually save the last one.
+            potential_declaration.append(t)
 
     if potential_declaration:
-        declarations.append(potential_declaration)
+        print(potential_declaration)
+        if type_found:
+            declarations.append(potential_declaration[-1])
+        elif len(potential_declaration) >= 2:
+            declarations.append(potential_declaration[-1])
 
-    if declarations:
-        return declarations
+    return declarations
 
 
 def is_valid_identifier(s):
