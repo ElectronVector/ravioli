c87aca72eb87c051214c9516454d1c5f5b6bdaa0
Matt <matt@electronvector.com>
2021-09-17 11:54:59 -0600

    Start extracting blocks of statements

--- ravioli/extract_statements.py
+++ ravioli/extract_statements.py
@@ -1,4 +1,15 @@
 
+class Block:
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return f"Block({self.title})"
+
+    def __eq__(self, other):
+        return self.title == other.title
+
+
 def extract_statements(code):
     statements = []
     current_statement = ""
@@ -7,6 +18,11 @@ def extract_statements(code):
             # Save the current statement.
             statements.append(clean_up_whitespace(current_statement))
             current_statement = ""
+        elif c == "{":
+            title = clean_up_whitespace(current_statement)
+            title = title.replace("(", "")
+            title = title.replace(")", "")
+            statements.append(Block(title))
         else:
             current_statement += c
 
--- test/test_extract_statements.py
+++ test/test_extract_statements.py
@@ -1,4 +1,5 @@
-from ravioli.extract_statements import extract_statements
+from ravioli.extract_statements import extract_statements, Block
+
 
 # TODO
 # Update statement extractor to extract blocks.
@@ -28,6 +29,14 @@ def test_multiple_statements_with_extra_whitespace():
             c;
     """
     assert extract_statements(code) == ["int a", "int b = 0", "int c"]
+
+
+def test_block():
+    code = """
+    function_def() {
+    }
+    """
+    assert extract_statements(code) == [Block("function_def")]
 
 # Test getting line numbers from statements.
 # TODO
