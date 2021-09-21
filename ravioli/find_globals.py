c430fcb62ce1839c33fe24b0493b9c1f1f32781e
Matt <matt@electronvector.com>
2021-09-21 13:55:11 -0600

    Comments

--- ravioli/find_globals.py
+++ ravioli/find_globals.py
@@ -33,6 +33,7 @@ def find_globals_by_function(code):
     statements = extract_statements(code)
     for s in statements:
         if isinstance(s, Block):
+            # The title includes the return type so we need to get the last word as the name of the function.
             functions[get_last_word(s.title)] = []
     return functions
 
