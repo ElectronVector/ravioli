79e89e6a6af6f8eb35d6eb9e647cc6ef657183d0
Matt <matt@electronvector.com>
2021-09-22 08:53:29 -0600

    Remove unused extraction function

--- ravioli/find_globals.py
+++ ravioli/find_globals.py
@@ -2,21 +2,6 @@ from ravioli.extract_declarations_and_usages import extract_declarations_from_st
 from ravioli.extract_statements import extract_statements, Block
 
 
-def extract_undefined_usages(code):
-    statements = extract_statements(code)
-    declarations = []
-    usages = []
-    for s in statements:
-        new_declarations = extract_declarations_from_statement(s)
-        if new_declarations:
-            declarations += new_declarations
-        new_usages = extract_usages_from_statement(s)
-        if new_usages:
-            usages += new_usages
-
-    return [u for u in usages if u not in declarations]
-
-
 def get_last_word(s):
     """
     Get the last whitespace delimited word in the string.
