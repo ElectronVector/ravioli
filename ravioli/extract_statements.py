5e73fef46bf455b7e8408de3e61a6983507b41a7
Matt <matt@electronvector.com>
2021-09-27 15:10:57 -0600

    Parse struct trailing content inside a function

--- ravioli/extract_statements.py
+++ ravioli/extract_statements.py
@@ -63,19 +63,20 @@ def extract_statements(code):
     parse_tree = []
     nest_levels = [parse_tree]
     temp = ""
+    last_block = None
     for c in code:
         if c == ";":
             # Save the current statement.
             text = clean_up_whitespace(temp)
             newlines_in_text = temp.count("\n")
-            # Todo: Make this a little less complicated.
             # If we just finished parsing a struct block, add the next semi-colon delimited statement as trailing
             # content to the previous block.
-            if parse_tree and isinstance(parse_tree[-1], Block) and "struct" in parse_tree[-1].title and not isinstance(nest_levels[-1], Block):
-                parse_tree[-1].trailing_context = text
+            if last_block and "struct" in last_block.title:
+                last_block.trailing_context = text
             else:
                 nest_levels[-1].append(Statement(text, line_number - newlines_in_text))
             temp = ""
+            last_block = None
         elif c == "{":
             title, params = extract_name_and_parameters(temp)
             title = clean_up_whitespace(title)
@@ -87,6 +88,7 @@ def extract_statements(code):
             nest_levels.append(new_block)
             temp = ""
         elif c == "}":
+            last_block = nest_levels[-1]
             # This is the end of a block. Remove this nest level as we aren't going to save anything here any more.
             nest_levels.pop()
             temp = ""
