e66d7ae56108340445f53e107ed2285fd924bbd6
Brian Herold <brian@carrot.co>
2022-09-06 13:12:01 -0700

    Strip mid-line comments

--- ravioli/strip_comments.py
+++ ravioli/strip_comments.py
@@ -1,7 +1,13 @@
 def strip_comments(code):
     # Remove single line comments.
     code_lines = code.splitlines(True)
-    non_comment_lines = [line for line in code_lines if not line.lstrip().startswith("//")]
+    non_comment_lines = list()
+    for line in code_lines:
+        if not "//" in line:
+            non_comment_lines.append(line)
+        elif not line.lstrip().startswith("//"):
+            comment_start = line.index("//")
+            non_comment_lines.append(line[0:comment_start]+"\n")
     code = "".join(non_comment_lines)
     # Remove block comments
     stripped = ""
